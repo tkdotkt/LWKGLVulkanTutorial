998dbb62f1849920fdda09ebe5839d4eba895c80
Thribhu Krishnan <thribhu.krishnan44@gmail.com>
2024-11-02 13:55:18 +1100

    Finished chapter 2, literally nothing on the screen, spent an entire 2hr 25 min on this.

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,15 @@ plugins {
 }
 
 application {
-    mainClass.set("tk.kt.LWKGLVulkanTutorial.Main") // Replace with your main class
+    mainClass.set("tk.kt.LWKGLVulkanTutorial.engine.MainKt") // Replace with your main class
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "tk.kt.LWKGLVulkanTutorial.engine.MainKt" // Replace with your main class
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 group = "tk.kt"
@@ -50,101 +58,36 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+    dependencies {
+        implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
 
-    implementation("org.lwjgl", "lwjgl")
-    implementation("org.lwjgl", "lwjgl-assimp")
-    implementation("org.lwjgl", "lwjgl-bgfx")
-    implementation("org.lwjgl", "lwjgl-cuda")
-    implementation("org.lwjgl", "lwjgl-egl")
-    implementation("org.lwjgl", "lwjgl-fmod")
-    implementation("org.lwjgl", "lwjgl-freetype")
-    implementation("org.lwjgl", "lwjgl-glfw")
-    implementation("org.lwjgl", "lwjgl-harfbuzz")
-    implementation("org.lwjgl", "lwjgl-hwloc")
-    implementation("org.lwjgl", "lwjgl-jawt")
-    implementation("org.lwjgl", "lwjgl-jemalloc")
-    implementation("org.lwjgl", "lwjgl-ktx")
-    implementation("org.lwjgl", "lwjgl-libdivide")
-    implementation("org.lwjgl", "lwjgl-llvm")
-    implementation("org.lwjgl", "lwjgl-lmdb")
-    implementation("org.lwjgl", "lwjgl-lz4")
-    implementation("org.lwjgl", "lwjgl-meow")
-    implementation("org.lwjgl", "lwjgl-meshoptimizer")
-    implementation("org.lwjgl", "lwjgl-msdfgen")
-    implementation("org.lwjgl", "lwjgl-nanovg")
-    implementation("org.lwjgl", "lwjgl-nfd")
-    implementation("org.lwjgl", "lwjgl-nuklear")
-    implementation("org.lwjgl", "lwjgl-odbc")
-    implementation("org.lwjgl", "lwjgl-openal")
-    implementation("org.lwjgl", "lwjgl-opencl")
-    implementation("org.lwjgl", "lwjgl-opengl")
-    implementation("org.lwjgl", "lwjgl-opengles")
-    implementation("org.lwjgl", "lwjgl-openvr")
-    implementation("org.lwjgl", "lwjgl-openxr")
-    implementation("org.lwjgl", "lwjgl-opus")
-    implementation("org.lwjgl", "lwjgl-ovr")
-    implementation("org.lwjgl", "lwjgl-par")
-    implementation("org.lwjgl", "lwjgl-remotery")
-    implementation("org.lwjgl", "lwjgl-rpmalloc")
-    implementation("org.lwjgl", "lwjgl-shaderc")
-    implementation("org.lwjgl", "lwjgl-spvc")
-    implementation("org.lwjgl", "lwjgl-sse")
-    implementation("org.lwjgl", "lwjgl-stb")
-    implementation("org.lwjgl", "lwjgl-tinyexr")
-    implementation("org.lwjgl", "lwjgl-tinyfd")
-    implementation("org.lwjgl", "lwjgl-tootle")
-    implementation("org.lwjgl", "lwjgl-vma")
-    implementation("org.lwjgl", "lwjgl-vulkan")
-    implementation("org.lwjgl", "lwjgl-xxhash")
-    implementation("org.lwjgl", "lwjgl-yoga")
-    implementation("org.lwjgl", "lwjgl-zstd")
-    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-bgfx", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-freetype", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-harfbuzz", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-hwloc", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-jemalloc", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-ktx", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-libdivide", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-llvm", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-lmdb", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-lz4", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-meow", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-meshoptimizer", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-msdfgen", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-nanovg", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-nfd", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-nuklear", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-opengles", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-openvr", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-openxr", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-opus", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-ovr", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-par", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-remotery", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-rpmalloc", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-shaderc", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-spvc", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-sse", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-tinyexr", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-tinyfd", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-tootle", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-vma", classifier = lwjglNatives)
-    if (lwjglNatives == "natives-macos" || lwjglNatives == "natives-macos-arm64") runtimeOnly("org.lwjgl", "lwjgl-vulkan", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-xxhash", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-yoga", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-zstd", classifier = lwjglNatives)
-    implementation("org.joml", "joml", jomlVersion)
-    implementation("org.joml", "joml-primitives", `joml-primitivesVersion`)
-    implementation("com.code-disaster.steamworks4j", "steamworks4j", steamworks4jVersion)
-    implementation("com.code-disaster.steamworks4j", "steamworks4j-server", `steamworks4j-serverVersion`)
+        implementation("org.lwjgl", "lwjgl")
+        implementation("org.lwjgl", "lwjgl-assimp")
+        implementation("org.lwjgl", "lwjgl-bgfx")
+        implementation("org.lwjgl", "lwjgl-glfw")
+        implementation("org.lwjgl", "lwjgl-nanovg")
+        implementation("org.lwjgl", "lwjgl-nuklear")
+        implementation("org.lwjgl", "lwjgl-openal")
+        implementation("org.lwjgl", "lwjgl-opengl")
+        implementation("org.lwjgl", "lwjgl-par")
+        implementation("org.lwjgl", "lwjgl-stb")
+        implementation("org.lwjgl", "lwjgl-vulkan")
+        runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+        runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+        runtimeOnly("org.lwjgl", "lwjgl-bgfx", classifier = lwjglNatives)
+        runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+        runtimeOnly("org.lwjgl", "lwjgl-nanovg", classifier = lwjglNatives)
+        runtimeOnly("org.lwjgl", "lwjgl-nuklear", classifier = lwjglNatives)
+        runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+        runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+        runtimeOnly("org.lwjgl", "lwjgl-par", classifier = lwjglNatives)
+        runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+        if (lwjglNatives == "natives-macos" || lwjglNatives == "natives-macos-arm64") runtimeOnly("org.lwjgl", "lwjgl-vulkan", classifier = lwjglNatives)
+        implementation("org.joml", "joml", jomlVersion)
+        implementation("org.joml", "joml-primitives", `joml-primitivesVersion`)
+    }
 
+    implementation("org.tinylog:tinylog-api:2.7.0")
     implementation("org.tinylog:tinylog-api-kotlin:2.7.0")
     implementation("org.tinylog:tinylog-impl:2.7.0")
 
@@ -176,34 +119,3 @@ sourceSets {
     }
 }
 
-tasks.register<Jar>("fatJar") {
-    archiveClassifier.set("all")
-    from(sourceSets.main.get().output)
-
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-    })
-
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-
-    manifest {
-        attributes["Main-Class"] = application.mainClass.get()
-    }
-}
-
-tasks.register<JavaExec>("customRun") {
-    group = "application"
-    description = "Runs the application with custom settings"
-    classpath = sourceSets.main.get().runtimeClasspath
-    mainClass.set(application.mainClass.get())
-    args = listOf("--dev") // Add any arguments you need here
-
-    doLast {
-        println("Application finished running successfully.")
-    }
-}
-
-tasks.build {
-    dependsOn(tasks.named("fatJar"))
-}
